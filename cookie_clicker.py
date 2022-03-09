10f94abc8efc62654bb541ab950abed14293614c
firemask8 <101260584+firemask8@users.noreply.github.com>
2022-03-09 23:12:19 +1100

    cookie_clicker.py

---
+++ cookie_clicker.py
@@ -0,0 +1,15 @@
+import win32api, win32con
+import time
+import keyboard
+
+def click():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    
+keyboard.wait("p")
+time.sleep(2)
+while True:
+    click()
+    if keyboard.is_pressed("p"):
+        break
